6ef0939e6d20f73fdfc84252e9c9e6616b856870
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-17 15:29:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
 end
 
-#def all_supplies_in_holidays(holiday_hash)
+def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holidays|
      puts "#{season.to_s.capitalize}:"
 
